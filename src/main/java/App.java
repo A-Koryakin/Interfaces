d02be670c48c49dc7a8e1d25528b10bd8979d6c4
Alexander Koryakin <sanek31rus@gmail.com>
2019-11-14 18:03:05 +0300

    Перешёл на Enum при создании объектов

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import ru.bstu.ieitus.vt41.kao.model.Construction;
+import ru.bstu.ieitus.vt41.kao.enums.ConstructionType;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,78 +8,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int constrNum;
-        System.out.print("Введите количество сооружений: ");
-        constrNum = scanner.nextInt();
 
         ArrayList<Construction> constructions = new ArrayList<>();
 
-        //TODO: Вынести создание объектов в фабричный метод
+        System.out.println("Доступные типы сооружений:");
+        System.out.print(ConstructionType.show());
+
+        System.out.print("Введите количество сооружений: ");
+        constrNum = scanner.nextInt();
 
         for (int i = 0; i < constrNum; i++) {
             int type;
-            System.out.print("Введите тип сооружения: 1 - Здание, 2 - Супермаркет, 3 - Частный дом,\n" +
-                    "4 - Многоквартирный дом, 5 - Путепровод, 6 - Мост, 7 - Тоннель: ");
+            System.out.print("Введите номер типа сооружения: ");
             type = scanner.nextInt();
-
-            switch (type) {
-                case 1: {
-                    System.out.println("Здание:");
-                    Building building = new Building();
-                    constructions.add(building);
-                    break;
-                }
-
-                case 2: {
-                    System.out.println("Супермаркет:");
-                    Supermarket supermarket = new Supermarket();
-                    constructions.add(supermarket);
-                    break;
-                }
-
-                case 3: {
-                    System.out.println("Частный дом:");
-                    PrivateHouse privateHouse = new PrivateHouse();
-                    constructions.add(privateHouse);
-                    break;
-                }
-
-                case 4: {
-                    System.out.println("Многоквартирный дом:");
-                    ApartmentHouse apartmentHouse = new ApartmentHouse();
-                    constructions.add(apartmentHouse);
-                    break;
-                }
-
-                case 5: {
-                    System.out.println("Путепровод:");
-                    Overpass overpass = new Overpass();
-                    constructions.add(overpass);
-                    break;
-                }
-
-                case 6: {
-                    System.out.println("Мост:");
-                    Bridge bridge = new Bridge();
-                    constructions.add(bridge);
-                    break;
-                }
-
-                case 7: {
-                    System.out.println("Тоннель:");
-                    Tunnel tunnel = new Tunnel();
-                    constructions.add(tunnel);
-                    break;
-                }
-                default: {
-                    System.out.println("Что-то пошло не так. Попробуйте ещё раз");
-                    i--;
-                    break;
-                }
-            }
+            constructions.add(ConstructionType.getByIndex(type).create());
         }
-
-        /*String minExp = getMin(constructions).toString();*/
 
         System.out.println("Сооружение с минимальным сроком эксплуатации:");
         System.out.println(Collections.min(constructions));
---
+++ src/main/java/ru/bstu/ieitus/vt41/kao/enums/ConstructionType.java
@@ -0,0 +1,76 @@
+package ru.bstu.ieitus.vt41.kao.enums;
+
+import lombok.Getter;
+import ru.bstu.ieitus.vt41.kao.model.*;
+
+public enum ConstructionType {
+    APARTMENT_HOUSE ("Многоквартирный дом"){
+        @Override
+        public ApartmentHouse create()
+        {
+            return new ApartmentHouse();
+        }
+
+    },
+    PRIVATE_HOUSE("Частный дом"){
+        @Override
+        public PrivateHouse create()
+        {
+            return new PrivateHouse();
+        }
+    },
+    SUPERMARKET ("Супермаркет"){
+        @Override
+        public Supermarket create()
+        {
+            return new Supermarket();
+        }
+    },
+    BRIDGE("Мост") {
+        @Override
+        public Bridge create()
+        {
+            return new Bridge();
+        }
+    },
+    TUNNEL ("Тоннель"){
+        @Override
+        public Tunnel create()
+        {
+            return new Tunnel();
+        }
+    };
+
+    @Getter
+    private String mName;
+    ConstructionType(String name) {mName = name;}
+
+    public abstract Construction create();
+
+    public static ConstructionType getByIndex(int index){
+        for(ConstructionType constructionType : ConstructionType.values())
+        {
+            if(constructionType.ordinal() == index)
+            {
+                return constructionType;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ordinal() + " - " + getMName() + "\n";
+    }
+
+    public static String show()
+    {
+        StringBuilder result = new StringBuilder();
+        for(ConstructionType constructionType : ConstructionType.values())
+        {
+            result.append(constructionType.toString());
+        }
+        return result.toString();
+    }
+}
